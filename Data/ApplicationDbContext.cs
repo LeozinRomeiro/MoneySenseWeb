12b89307d147123c175bf4fa315c083fa9fadb77
Leonardo Romeiro <leozinwork@gmail.com>
2023-11-09 13:17:51 -0300

    Ajustando

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using MoneySenseWeb.Models.Income;
 using MoneySenseWeb.Models.Expense;
 using MoneySenseWeb.Data.Mappings;
-using MoneySenseWeb.Models.Actors;
 using Microsoft.AspNetCore.Identity;
 using MoneySenseWeb.Models.ActorsContext;
+using MoneySenseWeb.Models.Income;
 
 namespace MoneySenseWeb.Data;
 
@@ -17,8 +16,8 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     }
     public DbSet<Salary> Salaries { get; set; }
 	public DbSet<Other> Others { get; set; }
-	public DbSet<MoneySenseWeb.Models.Income.Unexpected> IncomeUnexpecteds { get; set; }
-    public DbSet<MoneySenseWeb.Models.Expense.Unexpected> ExpenseUnexpecteds { get; set; }
+	public DbSet<Models.Income.Unexpected> IncomeUnexpecteds { get; set; }
+    public DbSet<Models.Expense.Unexpected> ExpenseUnexpecteds { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
 		//optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Data;User ID=sa;Password=Leoadmin32@#$;Trusted_Connection=False;TrustServerCertificate=True;");
