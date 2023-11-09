ae7dba884c992d7d199c27531515be741722b70d
Leonardo Romeiro <leozinwork@gmail.com>
2023-11-09 12:58:18 -0300

    Ajustado erro no Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ using MoneySenseWeb.Models.ActorsContext;
 
 namespace MoneySenseWeb.Data;
 
-public class ApplicationDbContext : IdentityDbContext<UserLogin>
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
@@ -21,17 +21,17 @@ public class ApplicationDbContext : IdentityDbContext<UserLogin>
     public DbSet<MoneySenseWeb.Models.Expense.Unexpected> ExpenseUnexpecteds { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
-		optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Data;User ID=sa;Password=Leoadmin32@#$;Trusted_Connection=False;TrustServerCertificate=True;");
+		//optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Data;User ID=sa;Password=Leoadmin32@#$;Trusted_Connection=False;TrustServerCertificate=True;");
 		optionsBuilder.UseSqlServer("Server=LEONARDOR-PC\\SQLEXPRESS;Database=DataHomologacao;User Id=leo_sa;Password=leosa32!;Trusted_Connection=False; TrustServerCertificate=True;");
 		//optionsBuilder.LogTo(Console.WriteLine);
 	}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfiguration(new SalaryMap());
         modelBuilder.ApplyConfiguration(new OtherMap());
         modelBuilder.ApplyConfiguration(new IncomeUnexpectedMap());
         modelBuilder.ApplyConfiguration(new ExpenseUnexpectedMap());
-        modelBuilder.ApplyConfiguration(new UserLoginMap());
     }
 }
