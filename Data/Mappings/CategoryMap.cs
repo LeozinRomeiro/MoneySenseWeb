3d1de619e1137ae4da0313f5c79dae8c1893f13e
Leonardo Romeiro <leozinwork@gmail.com>
2023-11-10 17:53:29 -0300

    Criando model category

---
+++ Data/Mappings/CategoryMap.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using MoneySenseWeb.Models;
+
+namespace MoneySenseWeb.Data.Mappings
+{
+	public class CategoryMap : IEntityTypeConfiguration<Category>
+	{
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			builder.ToTable("Category");
+			
+			builder.HasKey(x => x.Id);
+			
+			builder.Property(x=>x.Id)
+				.ValueGeneratedOnAdd()
+				.UseIdentityColumn();
+
+			builder.Property(x => x.Title)
+				.IsRequired()
+				.HasColumnName("Title")
+				.HasColumnType("NVARCAHR");
+
+			builder.Property(x => x.Icon)
+				.IsRequired()
+				.HasColumnName("Icon")
+				.HasColumnType("NVARCAHR");
+
+			builder.Property(x => x.Type)
+				.IsRequired()
+				.HasColumnName("Type")
+				.HasColumnType("NVARCAHR");
+		}
+	}
+}
---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneySenseWeb.Models
+{
+	public class Category
+	{
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string Type { get; set; }
+
+    }
+}
