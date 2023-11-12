0b64cd001042d07932c1146fb38bf0a123d1f806
Leonardo Romeiro <leozinwork@gmail.com>
2023-11-12 16:23:22 -0300

    Legendando Transaction

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,13 +1,22 @@
-﻿namespace MoneySenseWeb.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoneySenseWeb.Models
 {
     public class Transaction
     {
         public int TransactionId { get; set; }
         public int CategoryId { get; set; }
+        [DisplayName("Categoria")]
+        [Required(ErrorMessage = "Campo obrigatório, por favor aponte a categoria")]
         public Category Category { get; set; }
         public int Amount { get; set; }
+        [DisplayName("Descrição")]
         public string? Description { get; set; }
+        [DisplayName("Data")]
+        [Required(ErrorMessage = "Campo obrigatório, por favor indique a data da movimentação")]
         public DateTime Date {  get; set; }
+        [DisplayName("Registrado em")]
         public DateTime CreateDate { get; set; } = DateTime.Now;
     }
 }
