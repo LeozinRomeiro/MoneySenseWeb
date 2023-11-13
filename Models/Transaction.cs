d329266bd2579c086b363c86236b99862a90659e
Leonardo Romeiro <leozinwork@gmail.com>
2023-11-13 00:13:34 -0300

    Predefinindo a data como atual

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -15,7 +15,7 @@ namespace MoneySenseWeb.Models
         public string? Description { get; set; }
         [DisplayName("Data")]
         [Required(ErrorMessage = "Campo obrigatório, por favor indique a data da movimentação")]
-        public DateTime Date {  get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
         [DisplayName("Registrado em")]
         public DateTime CreateDate { get; set; } = DateTime.Now;
     }
